cf13e0a28dc6e37f6bb4b2ae78da11b276622f62
Serhat GECGIN <74310970+gecginserhat@users.noreply.github.com>
2024-09-22 20:58:30 +0300

    Update ui.js

--- ui.js
+++ ui.js
@@ -14,7 +14,7 @@ class UI {
        filmList.innerHTML += `
        
              <tr>
-                 <td><img src="${newFilm.url}" class="img-fluid img-thumbnail"></td>
+                 <td><img src="${newFilm.url}" style="width:100px" class="img-fluid img-thumbnail"></td>
                  <td>${newFilm.title}</td>
                  <td>${newFilm.director}</td>
                  <td><a href="#" id = "delete-film" class = "btn btn-danger">Delete Movie</a></td>
